2e5abe594af5e6834198eacc732abdc808f1c91f
markvhelp <mark.rahman@dynamicssoft.com.au>
2020-09-02 13:56:30 +1000

    First commit from Mark

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:fluttershare/pages/home.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // This is fix to start the app in emulator
+  Firestore.instance.settings(timestampsInSnapshotsEnabled: true).then((_) {
+    print("Timestamps enabled in snapshots\n");
+  }, onError: (_) {});
+  print("Error enabling timestamps in snapshots\n");
   runApp(MyApp());
 }
 
@@ -10,7 +18,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'FlutterShare',
       debugShowCheckedModeBanner: false,
-      home: Text("Hello World"),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        accentColor: Colors.teal,
+      ),
+      home: Home(),
     );
   }
 }
